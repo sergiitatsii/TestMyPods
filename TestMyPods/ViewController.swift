0199700223a6ab68b0dd174577fa49e664f82727
Sergii Tatsii <sergiitatsii@gmail.com>
2018-12-14 10:48:11 +0300

    Initial Commit

---
+++ TestMyPods/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestMyPods
+//
+//  Created by Sergii Tatsii on 12/14/18.
+//  Copyright © 2018 Sergii Tatsii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
